b533f441cd757907a4279d39ad63a98d15ede3ab
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2024-01-30 12:23:03 +0530

    Delete angular15 directory

--- angular15/src/app/directive/font-color.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FontColorDirective } from './font-color.directive';
-
-describe('FontColorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FontColorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- angular15/src/app/directive/italics-font.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ItalicsFontDirective } from './italics-font.directive';
-
-describe('ItalicsFontDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ItalicsFontDirective();
-    expect(directive).toBeTruthy();
-  });
-});
